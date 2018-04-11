0bd727ef621c8439e79af80baff16e3ad9a2e1b3
Junyoung Choi <fluke8259@gmail.com>
2018-04-12 05:04:30 +0900

    Implement error handler

---
+++ backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,27 @@
+import express = require('express')
+
+interface HTTPError extends Error {
+  code?: number
+  status?: number
+  isJoi?: boolean
+}
+
+export = function createErrorHandler (): express.ErrorRequestHandler {
+  return function errorHandler (error: HTTPError, req: express.Request, res: express.Response, next: express.NextFunction) {
+
+    const status = error.status
+      ? error.status
+      : (error.name === 'ValidationError' || error.isJoi)
+      ? 422
+      : 500
+
+    if (error.status === 500) {
+      console.error(error, error.code, error.name)
+    }
+
+    res.status(status)
+    res.json({
+      message: error.message
+    })
+  }
+}
