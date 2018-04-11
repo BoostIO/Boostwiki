46048fe13e86d5a064c03eac493e458920fc89e4
Junyoung Choi <fluke8259@gmail.com>
2018-04-12 04:34:59 +0900

    Use BACKEND_PORT for backend server on development

--- backend/src/scripts/runServer.ts
+++ backend/src/scripts/runServer.ts
@@ -1,9 +1,8 @@
 import express = require('express')
 import rootRouter = require('../rootRouter')
-import configuration = require('../configuration')
 
 const app = express()
 
 app.use(rootRouter)
 
-app.listen(configuration.port)
+app.listen(process.env.BACKEND_PORT)
--- server.js
+++ server.js
@@ -34,12 +34,12 @@ nextApp.prepare()
 
     if (dev) {
       const proxy = require('http-proxy-middleware')
-      expressApp.use('/api', proxy('http://127.0.0.1:3001'))
-      expressApp.use('/auth', proxy('http://127.0.0.1:3001'))
-      expressApp.use('/files', proxy('http://127.0.0.1:3001'))
+      expressApp.use('/api', proxy(`http://127.0.0.1:${process.env.BACKEND_PORT}`))
+      expressApp.use('/ws', proxy(`http://127.0.0.1:${process.env.BACKEND_PORT}`))
+      expressApp.use('/files', proxy(`http://127.0.0.1:${process.env.BACKEND_PORT}`))
     } else {
       const rootRouter = require('./backend/build/rootRouter')
-      expressApp.all(/\/api|\/auth|\/ws/, (req, res, next) => {
+      expressApp.all(/\/api|\/ws|\/files/, (req, res, next) => {
         req.url = req.baseUrl + req.url
         req.path = req.baseUrl + req.path
         req.baseUrl = '/'
