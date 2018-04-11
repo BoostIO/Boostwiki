b50cc51d7dbe961ad13f009c2f1e0498c1187621
Junyoung Choi <fluke8259@gmail.com>
2018-04-12 04:56:09 +0900

    Prepare configuration

--- backend/src/configuration.ts
+++ backend/src/configuration.ts
@@ -1,5 +1,51 @@
-const configuration = {
-  port: process.env.BACKEND_PORT || 3001
+import dotenv = require('dotenv')
+const dev = process.env.NODE_ENV !== 'production'
+const isTest = process.env.NODE_ENV === 'test'
+
+if (dev) {
+  if (isTest) {
+    dotenv.config({
+      path: 'test.env'
+    })
+  } else {
+    dotenv.config()
+  }
+} else {
+  dotenv.config({
+    path: 'production.env'
+  })
 }
 
-export = configuration
+export = {
+  nodeEnv: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3001,
+  baseURL: process.env.BASE_URL || 'http://localhost:3001',
+  webURL: process.env.WEB_URL || 'http://localhost:3000',
+  session: {
+    secret: process.env.SESSION_SECRET || 'elided',
+    redisPort: parseInt(process.env.SESSION_REDIS_PORT, 10) || 6379,
+    redisHost: process.env.SESSION_REDIS_HOST || 'localhost',
+    redisDB: parseInt(process.env.SESSION_REDIS_DB, 10) || 0
+  },
+  ws: {
+    redisPort: parseInt(process.env.WS_REDIS_PORT, 10) || 6379,
+    redisHost: process.env.WS_REDIS_HOST || 'localhost'
+  },
+  dbURL: process.env.DB_URL || 'mongodb://localhost',
+  cache: {
+    redisPort: parseInt(process.env.CACHE_REDIS_PORT, 10) || 6379,
+    redisHost: process.env.CACHE_REDIS_HOST || 'localhost',
+    redisDB: parseInt(process.env.CACHE_REDIS_DB, 10) || 1
+  },
+  github: {
+    clientID: process.env.GITHUB_CLIENT_ID || 'elided',
+    clientSecret: process.env.GITHUB_CLIENT_SECRET || 'elided'
+  },
+  s3: {
+    bucket: process.env.S3_BUCKET || 'elided',
+    region: process.env.S3_REGION || 'elided',
+    accessKey: process.env.S3_ACCESS_KEY || 'elided',
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'elided'
+  },
+  adminPassword: process.env.ADMIN_PASSWORD || 'elided'
+}
