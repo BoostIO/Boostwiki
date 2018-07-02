af76ab50ec87072ff1e7eadc21e943e369b290d0
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-02 18:18:29 +0900

    apply style

--- components/TopNavigator.tsx
+++ components/TopNavigator.tsx
@@ -4,37 +4,58 @@ import { CurrentUserState } from '../lib/CurrentUserState'
 import { RouteState } from '../lib/RouteState'
 import {
   AppBar,
-  Typography
+  Typography,
+  Avatar,
+  Button,
+  Toolbar
 } from '@material-ui/core'
+import {
+  withStyles,
+  WithStyles
+} from '@material-ui/core/styles'
 
 interface TopNavigatorProps {
   currentUser?: CurrentUserState
   route?: RouteState
 }
 
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: 1
+  }
+}
+
+type ClassNames = keyof typeof styles
+
 @inject('route')
 @inject('currentUser')
 @observer
-class TopNavigator extends React.Component<TopNavigatorProps> {
+class TopNavigator extends React.Component<TopNavigatorProps & WithStyles<ClassNames>> {
   public render () {
     const {
       currentUser,
-      route
+      route,
+      classes
     } = this.props
     return (
-      <div>
+      <div className={classes.root}>
         <AppBar color='primary' position='fixed'>
-          <Typography variant='title' color='inherit'>
-            Boostwiki
-          </Typography>
-          {currentUser == null
-            ? <a href='/auth/github'>Sign in</a>
-            : <img src={`https://avatars3.githubusercontent.com/u/${currentUser.githubId}?v=4&s=30`} />
-          }
+          <Toolbar>
+            <Typography variant='title' color='inherit' className={classes.flex}>
+              Boostwiki
+            </Typography>
+            {currentUser == null
+              ? <Button href='/auth/github' color='inherit'>Sign in</Button>
+              : <Avatar src={`https://avatars3.githubusercontent.com/u/${currentUser.githubId}?v=4&s=30`} />
+            }
+          </Toolbar>
         </AppBar>
       </div>
     )
   }
 }
 
-export default TopNavigator
+export default withStyles(styles)(TopNavigator)
