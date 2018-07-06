b5c9c0023fa477bedd91071d5efdcdf88df873d4
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-06 12:52:10 +0900

    Prepare rootRouter

--- server/rootRouter.ts
+++ server/rootRouter.ts
@@ -4,10 +4,6 @@ import session from 'express-session'
 import passport from './lib/passport'
 import createRedisStore from 'connect-redis'
 import configuration from './configuration'
-import { NotFoundError } from './lib/errors'
-import errorHandler from './middlewares/errorHandler'
-import apiRouter from './routes/api'
-import authRouter from './routes/auth'
 
 const router = express.Router()
 
@@ -30,24 +26,5 @@ router.use(session({
 }))
 router.use(passport.initialize())
 router.use(passport.session())
-
-router.get('/', (req, res) => {
-  res.send('root')
-})
-
-router.use('/api', apiRouter)
-router.use('/auth', authRouter)
-
-router.get('/ws', (req, res) => {
-  res.send('ws')
-})
-
-router.use(function (req, res, next) {
-  const err = new NotFoundError('The api doesn\'t exist.')
-
-  next(err)
-})
-
-router.use(errorHandler())
 
 export default router
