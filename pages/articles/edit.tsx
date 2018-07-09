ff6907270c63fee7d5ef763fa46e7f4ffa20818e
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-09 17:18:44 +0900

    Add Editor Component

--- pages/articles/edit.tsx
+++ pages/articles/edit.tsx
@@ -1,25 +1,68 @@
 import React from 'react'
 import { RootProps } from '../../lib/RootProps'
-import { withPageBundle, BundleContainerProps } from '../../lib/withPageBundle'
+import {
+  withPageBundle,
+  BundleContainerProps
+} from '../../lib/withPageBundle'
+import {
+  Button
+} from '@material-ui/core'
 
 interface ArticleEditQuery {
   keyword: string
 }
 
-@withPageBundle
-export default class ArticleEdit extends React.Component<RootProps<ArticleEditQuery> & BundleContainerProps> {
+interface ArticleEditState {
+  content: string
+}
+
+class ArticleEdit extends React.Component<RootProps<ArticleEditQuery> & BundleContainerProps, ArticleEditState> {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      content: ''
+    }
+  }
+
+  setContent = (content) => this.setState({ content })
+
+  handleEditorOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setContent(e.currentTarget.value)
+  }
+
+  handleOnClickSubmitButton () {
+    const {
+      content
+    } = this.state
+
+    console.log(content)
+  }
+
   render (): JSX.Element {
     const {
-      query,
-      pageProps
+      query
     } = this.props
+    const {
+      content
+    } = this.state
 
-    const { article } = pageProps
+    console.log(this.props)
 
     return (
       <>
         <h1>{query.keyword}</h1>
+        <textarea
+          value={content}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+            this.handleEditorOnChange(e)
+          }} />
+        <Button onClick={this.handleOnClickSubmitButton.bind(this)}>
+          Push
+        </Button>
       </>
     )
   }
 }
+
+export default withPageBundle(ArticleEdit)
