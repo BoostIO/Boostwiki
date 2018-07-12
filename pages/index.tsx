15dc6b42e53708f99df88db4a09cb8c27f496952
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-12 16:37:20 +0900

    Fix Toppage grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 import { withPageBundle } from '../lib/withPageBundle'
 import { Article } from '../lib/models'
 import ArticleCard from '../components/ArticleCard'
+import { withStyles, WithStyles } from '@material-ui/core/styles'
+import { Grid } from '@material-ui/core'
+
+const styles = {
+  root: {
+    maxWidth: 880,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    paddingTop: 16
+  }
+}
+
+type ClassNames = keyof typeof styles
 
 interface TopPageProps {
   pageProps: {
@@ -9,20 +22,21 @@ interface TopPageProps {
   }
 }
 
-const TopComponent: React.SFC<TopPageProps> = ({ pageProps }) => {
+const TopComponent: React.SFC<TopPageProps & WithStyles<ClassNames>> = ({ pageProps, classes }) => {
   const { articles } = pageProps
   return (
-    <>
+    <Grid container className={classes.root} spacing={16} justify='space-between'>
       {articles.length > 0
         ? articles.map(article => (
-          <ArticleCard
-            key={article._id}
-            article={article}
-          />
+          <Grid item key={article._id}>
+            <ArticleCard
+              article={article}
+            />
+          </Grid>
         ))
         : <p>There is no article.</p>}
-    </>
+    </Grid>
   )
 }
 
-export default withPageBundle(TopComponent)
+export default withStyles(styles)(withPageBundle(TopComponent))
