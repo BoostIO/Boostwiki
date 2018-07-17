adb188ea001eb7f132b93cd088b70c39ebb13396
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-17 16:51:07 +0900

    Apply styles to profile page

--- pages/users/show.tsx
+++ pages/users/show.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { User, Article } from '../../lib/models'
 import { withPageBundle } from '../../lib/withPageBundle'
-import { Avatar } from '@material-ui/core'
+import {
+  Avatar,
+  Typography
+} from '@material-ui/core'
 import Link from 'next/link'
+import {
+  withStyles,
+  WithStyles,
+  createStyles,
+  Theme
+} from '@material-ui/core/styles'
 
 interface UsersShowProps {
   pageProps: {
@@ -11,29 +20,57 @@ interface UsersShowProps {
   }
 }
 
-class UsersShow extends React.Component<UsersShowProps> {
+const styles = ({ palette }: Theme) => createStyles({
+  root: {
+    width: 1350,
+    margin: '0 auto'
+  },
+  anchor: {
+    color: palette.secondary.main
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    marginRight: 30
+  },
+  userInfo: {
+    display: 'flex',
+    padding: '20px 0'
+  }
+})
+
+type ClassNames = typeof styles
+
+class UsersShow extends React.Component<UsersShowProps & WithStyles<ClassNames>> {
   render () {
-    const { pageProps } = this.props
+    const { pageProps, classes } = this.props
     const { user, articles } = pageProps
     return (
-      <>
-       <h1>{user.displayName}</h1>
-       <h2>{user.uniqueName}</h2>
-       <Avatar src={user.photo} />
-       { articles.length > 0
-        ? articles.map(article => (
-          <p key={article._id}>
-            <Link href={`/articles/show?keyword=${article.keyword}`} as={`/w/${article.keyword}`}>
-              <a>
-                {article.keyword}
-              </a>
-            </Link>
-          </p>
-        ))
-        : <p>There is no article.</p>}
-      </>
+      <div className={classes.root}>
+        <div className={classes.userInfo}>
+          <Avatar src={user.photo} className={classes.avatar}/>
+          <div>
+            <Typography variant='display1'>{user.displayName}</Typography>
+            <Typography variant='headline'>{user.uniqueName}</Typography>
+          </div>
+        </div>
+        { articles.length > 0
+          ? <>
+              <Typography variant='title'>commited articles</Typography>
+              {articles.map(article => (
+                <Typography key={article._id} variant='subheading'>
+                  <Link href={`/articles/show?keyword=${article.keyword}`} as={`/w/${article.keyword}`}>
+                    <a className={classes.anchor}>
+                      {article.keyword}
+                    </a>
+                  </Link>
+                </Typography>
+              ))}
+          </>
+          : <Typography variant='subheading'>There is no article.</Typography>}
+      </div>
     )
   }
 }
 
-export default withPageBundle(UsersShow)
+export default withStyles(styles)(withPageBundle(UsersShow))
