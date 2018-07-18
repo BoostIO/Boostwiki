a4ca60073900c706e4731a9f3c1f5e2dbf324088
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-18 15:20:29 +0900

    Add /api/pages test

---
+++ server/specs/routes/pages/index.spec.ts
@@ -0,0 +1,28 @@
+import chai from 'chai'
+import express from 'express'
+import appRouter from '../../../appRouter'
+import { createArticle, tearDown } from '../../helpers/dummy'
+import range from 'lodash/range'
+
+describe('/api/pages/index', () => {
+  let agent
+  beforeEach(async () => {
+    const expressApp = express()
+    expressApp.use(appRouter)
+    agent = chai.request.agent(expressApp)
+  })
+
+  it('response latest 20 articles', async () => {
+    await Promise.all(
+      range(20).map(i => createArticle())
+    )
+
+    const res = await agent
+      .get('/api/pages')
+
+    expect(res.status).toBe(200)
+    expect(res.body.articles.length).toBe(20)
+  })
+
+  afterEach(tearDown)
+})
