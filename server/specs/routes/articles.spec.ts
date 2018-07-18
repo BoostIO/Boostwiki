f7db59abdb8f5b3cb24d3f0af049820ebecf9bc8
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-18 14:26:26 +0900

    Add /api/articles tests

---
+++ server/specs/routes/articles.spec.ts
@@ -0,0 +1,64 @@
+import chai from 'chai'
+import express from 'express'
+import app from '../../app'
+import User from '../../models/User'
+import { createUser, createArticle, tearDown } from '../helpers/dummy'
+
+describe('/api/articles', () => {
+  let user: User
+  let agent
+  beforeEach(async () => {
+    user = await createUser()
+
+    const expressApp = express()
+    expressApp.use(app)
+    agent = chai.request.agent(expressApp)
+  })
+
+  it('response new article.', async () => {
+    await agent.get(`/test/signin/${user._id}`)
+
+    const res = await agent
+      .post('/api/articles')
+      .send({
+        keyword: 'test_keyword',
+        content: 'test content'
+      })
+
+    expect(res.status).toBe(200)
+    expect(res.body).toMatchObject({
+      article: {
+        keyword: 'test_keyword',
+        headCommit: {
+          content: 'test content',
+          user: user._id.toString()
+        }
+      }
+    })
+  })
+
+  it('response updated article', async () => {
+    await agent.get(`/test/signin/${user._id}`)
+
+    await createArticle({ keyword: 'test_keyword' })
+
+    const res = await agent
+      .put('/api/articles')
+      .send({
+        keyword: 'test_keyword',
+        content: 'update content'
+      })
+
+    expect(res.status).toBe(200)
+    expect(res.body).toMatchObject({
+      article: {
+        keyword: 'test_keyword',
+        headCommit: {
+          content: 'update content'
+        }
+      }
+    })
+  })
+
+  afterEach(tearDown)
+})
