fe71ace3ad7b98f76da84cafc12baae4adde24d0
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-20 13:18:21 +0900

    Implement component for show rendered markdown

---
+++ components/MarkdownPreview.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import marked from 'marked'
+
+interface MarkdownPreviewProps {
+  content: string
+  className: string
+}
+
+export default class MarkdownPreview extends React.Component<MarkdownPreviewProps> {
+  render () {
+    const { content, className } = this.props
+    const renderer = new marked.Renderer()
+    renderer.link = (href, title, text) => (
+      `<a target="_blank" rel="noopener noreferrer" href="${href}" title="${title}">${text}</a>`
+    )
+    const html = marked(content, { renderer })
+    return (
+      <div className={className} dangerouslySetInnerHTML={{ __html: html }} />
+    )
+  }
+}
