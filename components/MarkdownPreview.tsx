3221d493048bc3fa95fe41623af47558da89f316
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-21 13:07:10 +0900

    Refactor component of MarkdownPreview

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import marked from 'marked'
 import classnames from 'classnames'
-import Highlighter from './Highlighter'
+import hljs from 'highlight.js'
 import {
   withStyles,
   WithStyles,
@@ -28,8 +28,29 @@ type ClassNames = typeof styles
 class MarkdownPreview extends React.Component<MarkdownPreviewProps & WithStyles<ClassNames>> {
   static defaultProps: MarkdownPreviewProps = { content: '' }
 
+  componentDidMount () {
+    this.highlightCode()
+  }
+
+  componentDidUpdate () {
+    this.highlightCode()
+  }
+
+  element = null
+
+  highlightCode = () => {
+    const nodes = this.element.querySelectorAll('pre code')
+
+    for (let i = 0; i < nodes.length; i++) {
+      hljs.highlightBlock(nodes[i])
+    }
+  }
+
+  setElement = (element: Element) => this.element = element
+
   render () {
     const { content, className, classes } = this.props
+
     const renderer = new marked.Renderer()
     renderer.link = (href, title, text) => (
       `<a target="_blank" rel="noopener noreferrer" href="${href}" title="${title}" class="${classnames(classes.anchor)}">${text}</a>`
@@ -38,8 +59,15 @@ class MarkdownPreview extends React.Component<MarkdownPreviewProps & WithStyles<
       `<pre><code class="${classnames(classes.code)}">${code}</code></pre>`
     )
     const html = marked(content, { renderer })
+
     return (
-      <Highlighter content={html} className={className} />
+      <div
+        ref={this.setElement}
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: html
+        }}
+      />
     )
   }
 }
