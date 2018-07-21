291d36a7dcae37aa5542ba8f4eef08355d0c308e
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-21 13:56:48 +0900

    Refactor configuration

--- server/configuration.ts
+++ server/configuration.ts
@@ -18,34 +18,16 @@ if (dev) {
 
 export default {
   nodeEnv: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3001,
   baseURL: process.env.BASE_URL || 'http://localhost:3000',
-  webURL: process.env.WEB_URL || 'http://localhost:3000',
   session: {
     secret: process.env.SESSION_SECRET || 'elided',
     redisPort: parseInt(process.env.SESSION_REDIS_PORT, 10) || 6379,
     redisHost: process.env.SESSION_REDIS_HOST || 'localhost',
     redisDB: parseInt(process.env.SESSION_REDIS_DB, 10) || 0
   },
-  ws: {
-    redisPort: parseInt(process.env.WS_REDIS_PORT, 10) || 6379,
-    redisHost: process.env.WS_REDIS_HOST || 'localhost'
-  },
   dbURL: process.env.DB_URL || 'mongodb://localhost',
-  cache: {
-    redisPort: parseInt(process.env.CACHE_REDIS_PORT, 10) || 6379,
-    redisHost: process.env.CACHE_REDIS_HOST || 'localhost',
-    redisDB: parseInt(process.env.CACHE_REDIS_DB, 10) || 1
-  },
   github: {
     clientID: process.env.GITHUB_CLIENT_ID || 'elided',
     clientSecret: process.env.GITHUB_CLIENT_SECRET || 'elided'
-  },
-  s3: {
-    bucket: process.env.S3_BUCKET || 'elided',
-    region: process.env.S3_REGION || 'elided',
-    accessKey: process.env.S3_ACCESS_KEY || 'elided',
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'elided'
-  },
-  adminPassword: process.env.ADMIN_PASSWORD || 'elided'
+  }
 }
